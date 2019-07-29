83d2c1904f508553e23c3272df884ab30d054802
Rehman Ali <rehmanali001@gmail.com>
2019-07-29 20:18:59 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(string)
   reversed = []
   new_string = string.split(" ")
   new_string.collect do |line|
-  reversed << line.reverse
+  reversed.push(line.reverse)
 end
   reversed.join(" ")
 end
